eb456187b76a948d6f87d976ddde18878d147939
wtjerry <wtjerry123@gmail.com>
2016-10-19 16:47:29 +0200

    usermanagement screen now shows buttons to lock / unlock users

--- js/userManagement.js
+++ js/userManagement.js
@@ -3,14 +3,18 @@ var UserManagement = function() {
 };
 
 UserManagement.prototype.setUsers = function(users) {
-    var s = '<form name="users">';
+
+    var checkboxes = [];
     for(var i=0;i<users.length;i++){
         $user = users[i];
         $nameAndLockedState = $user.name + ' ' + $user.isLocked;
-        s += '<input type="checkbox" name="u" value="' + $user.name + '">' + $nameAndLockedState + '</input>';
-        s += '<br>';
+        var s = '<input type="checkbox" name="u" value="' + $user.name + '">' + $nameAndLockedState + '</input>';
+        checkboxes.push(s);
     }
-    s+= '</form>';
 
-    $('#userManagement').html(s);
+    var unlockButton = '<input type="submit" class="blueButton" value="Unlock checked users" />';
+    var lockButton = '<input type="submit" class="blueButton" value="Lock checked users" />';
+    var html = checkboxes.join('<br>') + '<br>' + unlockButton + lockButton;
+
+    $('#users').html(html);
 }
