b08ca68cd68ca3b021337438995d57db9935ef2a
wtjerry <wtjerry123@gmail.com>
2016-11-05 19:14:13 +0100

    refactoring: reformated class chat

--- php/classes/Chat.class.php
+++ php/classes/Chat.class.php
@@ -4,9 +4,9 @@
 
 class Chat{
 
-	public static function register($name,$email,$password,$passwordReenter){
+    public static function register($name,$email,$password,$passwordReenter) {
 
-	    if(!$name || !$email){
+        if(!$name || !$email){
             throw new Exception('Fill in all the required fields.');
         }
 
@@ -29,9 +29,9 @@ class Chat{
         $hashAndSalt = password_hash($password, PASSWORD_BCRYPT);
 
         $user = new ChatUser(array(
-            'name'		=> $name,
+            'name'              => $name,
             'hashAndSalt'  => $hashAndSalt,
-            'gravatar'	=> $gravatar
+            'gravatar'          => $gravatar
         ));
 
         // The save method returns a MySQLi object
@@ -40,88 +40,97 @@ class Chat{
         }
 
         return array('status' => 1);
-	}
+    }
 
-	public static function login($name,$password){
-		if(!$name || !$password){
-			throw new Exception('Fill in all the required fields.');
-		}
+    public static function login($name,$password) {
+        if(!$name || !$password){
+            throw new Exception('Fill in all the required fields.');
+        }
 
         $user = UserDB::loginUserOrThrow($name,$password);
 
-		$_SESSION['user']	= array(
-			'name'		=> $user->getName(),
-			'gravatar'	=> $user->getGravatar()
-		);
+        $_SESSION['user']	= array(
+                'name'	=> $user->getName(),
+                'gravatar'	=> $user->getGravatar()
+        );
 
-		return array(
-			'status'	=> 1,
-			'name'		=> $user->getName(),
-			'gravatar'	=> $user->getGravatarFromHash()
-		);
-	}
+        return array(
+                'status'	=> 1,
+                'name'	=> $user->getName(),
+                'gravatar'	=> $user->getGravatarFromHash()
+        );
+    }
 	
-	public static function checkLogged(){
-		$response = array('logged' => false);
-			
-		if($_SESSION['user']['name']){
-			$response['logged'] = true;
-			$response['loggedAs'] = array(
-				'name'		=> $_SESSION['user']['name'],
-				'gravatar'	=> Converter::convertHashToGravatar($_SESSION['user']['gravatar'])
-			);
-		}
-		
-		return $response;
-	}
+    public static function checkLogged() {
+        $response = array('logged' => false);
+
+        if($_SESSION['user']['name']) {
+            $response['logged']     = true;
+            $response['loggedAs'] = array(
+                    'name'                  => $_SESSION['user']['name'],
+                    'gravatar'              => Converter::convertHashToGravatar($_SESSION['user']['gravatar'])
+            );
+        }
+
+        return $response;
+    }
 	
+<<<<<<< HEAD
 	public static function logout(){
         UserDB::logout(OldDB::esc($_SESSION['user']['name']));
 		
 		$_SESSION = array();
 		unset($_SESSION);
+=======
+    public static function logout() {
+        UserDB::logout(NewDB::esc($_SESSION['user']['name']));
+>>>>>>> eccca4e... refactoring: reformated class chat
 
-		return array('status' => 1);
-	}
-	
-	public static function submitChat($chatText){
-		if(!$_SESSION['user']){
-			throw new Exception('You are not logged in');
-		}
-		
-		if(!$chatText){
-			throw new Exception('You haven\' entered a chat message.');
-		}
-	
-		$chat = new ChatLine(array(
-			'author'	=> $_SESSION['user']['name'],
-			'gravatar'	=> $_SESSION['user']['gravatar'],
-			'text'		=> $chatText
-		));
-	
-		// The save method returns a MySQLi object
-		$insertID = $chat->save()->insert_id;
-	
-		return array(
-			'status'	=> 1,
-			'insertID'	=> $insertID
-		);
-	}
+        $_SESSION = array();
+        unset($_SESSION);
 
-	public static function getUsers(){
-		if($_SESSION['user']['name']){
-			$user = new ChatUser(array('name' => $_SESSION['user']['name']));
-			$user->update();
-		}
+        return array('status' => 1);
+    }
+	
+    public static function submitChat($chatText){
+        if(!$_SESSION['user']){
+            throw new Exception('You are not logged in');
+        }
 
-		// Deleting chats older than 5 minutes and users inactive for 30 seconds
-		OldDB::query("DELETE FROM webchat_lines WHERE ts < SUBTIME(NOW(),'0:5:0')");
-		UserDB::logoutInactiveUsers();
+        if(!$chatText){
+            throw new Exception('You haven\' entered a chat message.');
+        }
 
-		$usersDB = UserDB::getLoggedInUsers();
+        $chat = new ChatLine(array(
+                'author'	=> $_SESSION['user']['name'],
+                'gravatar'	=> $_SESSION['user']['gravatar'],
+                'text'	=> $chatText
+        ));
+
+        // The save method returns a MySQLi object
+        $insertID = $chat->save()->insert_id;
+
+        return array(
+                'status'	=> 1,
+                'insertID'	=> $insertID
+        );
+    }
+
+    public static function getUsers() {
+        if($_SESSION['user']['name']){
+                $user = new ChatUser(array('name' => $_SESSION['user']['name']));
+                $user->update();
+        }
+
+        // Deleting chats older than 5 minutes and users inactive for 30 seconds
+        OldDB::query("DELETE FROM webchat_lines WHERE ts < SUBTIME(NOW(),'0:5:0')");
+
+        UserDB::logoutInactiveUsers();
+
+        $usersDB = UserDB::getLoggedInUsers();
 
         $users = array();
-        foreach($usersDB as $u){
+        foreach($usersDB as $u) {
             $user = array(
                 'name' => $u->getName(),
                 'gravatar' => $u->getGravatarFromHash()
@@ -129,36 +138,36 @@ class Chat{
             $users[] = $user;
         }
 
-		return array(
-			'users' => $users,
-			'total' => count($users)
-		);
-	}
+        return array(
+                'users' => $users,
+                'total' => count($users)
+        );
+    }
 	
-	public static function getChats($lastID){
-		$lastID = (int)$lastID;
-	
-		$result = OldDB::query('SELECT * FROM webchat_lines WHERE id > '.$lastID.' ORDER BY id ASC');
-	
-		$chats = array();
-		if($result) {
-			while($chat = $result->fetch_object()){
-		
-				// Returning the GMT (UTC) time of the chat creation:
-		
-				$chat->time = array(
-					'hours'		=> gmdate('H',strtotime($chat->ts)),
-					'minutes'	=> gmdate('i',strtotime($chat->ts))
-				);
-		
-				$chat->gravatar = Converter::convertHashToGravatar($chat->gravatar);
-		
-				$chats[] = $chat;
-			}
-		}
-	
-		return array('chats' => $chats);
-	}
+    public static function getChats($lastID){
+        $lastID = (int)$lastID;
+
+        $result = OldDB::query('SELECT * FROM webchat_lines WHERE id > '.$lastID.' ORDER BY id ASC');
+
+        $chats = array();
+        if($result) {
+            while($chat = $result->fetch_object()){
+
+                // Returning the GMT (UTC) time of the chat creation:
+
+                $chat->time = array(
+                        'hours'		=> gmdate('H',strtotime($chat->ts)),
+                        'minutes'	=> gmdate('i',strtotime($chat->ts))
+                );
+
+                $chat->gravatar = Converter::convertHashToGravatar($chat->gravatar);
+
+                $chats[] = $chat;
+            }
+        }
+
+        return array('chats' => $chats);
+    }
 }
 
 
